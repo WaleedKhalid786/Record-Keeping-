cbe27b4528ae792f2fa0aa409976189a30a2c341
Waleed <142690768+WaleedKhalid786@users.noreply.github.com>
2023-12-02 16:01:27 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './component/header'
+import Waled from './component/waleed'
+import './App.css'
+import './component/field'
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Waled></Waled>
+    </div>
+  )
+}
+
+export default App
---
+++ header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="Header">
+        <center><h1>Record Keeping</h1></center>
+    </div>
+  )
+}
+
+export default Header
